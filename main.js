d1d0da841cdbe4d22766b221a38512f9143b5899
Creezelr <167479123+Creezelr@users.noreply.github.com>
2026-04-05 17:33:25 -0500

    Remove audio playback event listener
    Remove audio play functionality from main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 
-const audio = document.getElementById("music");
-const button = document.getElementById("playBtn");
-
-button.addEventListener("click", () => {
-  audio.volume = 1;
-  audio.muted = false;
-  audio.play();
-});
