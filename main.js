f0e102cb65168c135ffbc6aebb83262d3c27de9a
Creezelr <167479123+Creezelr@users.noreply.github.com>
2026-04-05 17:22:00 -0500

    Add audio playback functionality on button click

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 
+const audio = document.getElementById("soviet-anthem.mp3");
+const button = document.getElementById("playBtn");
+
+button.addEventListener("click", () => {
+  audio.play();
+  audio.loop = true;
+});
