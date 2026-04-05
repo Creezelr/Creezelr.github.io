c0af01f1267d4825cf67eb6f83e4052d2c495112
Creezelr <167479123+Creezelr@users.noreply.github.com>
2026-04-05 17:30:30 -0500

    Set audio volume and unmute on play button click

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ const audio = document.getElementById("music");
 const button = document.getElementById("playBtn");
 
 button.addEventListener("click", () => {
+  audio.volume = 1;
+  audio.muted = false;
   audio.play();
 });
