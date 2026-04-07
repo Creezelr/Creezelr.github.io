6acd5a95d1440edd6b66d69f9def1b376d87c0a7
Creezelr <167479123+Creezelr@users.noreply.github.com>
2026-04-06 19:17:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,6 @@
 let USSRAnthem = document.getElementById("music");
 let btn = document.getElementById("playBtn");
 
-function warshipSong(){  
-  music.play();
-}
-
 btn.onclick = function(){
   USSRAnthem.play();
 }
