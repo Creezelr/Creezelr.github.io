9eb45dadf9684a351a2ae85a49db3be2c67967e2
Creezelr <167479123+Creezelr@users.noreply.github.com>
2026-04-11 21:21:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+const ball = document.getElementById('ball');
+
+window.addEventListener('mousemove', (e) => {
+
+  const x = e.clientX;
+  const y = e.clientY;
+  
+
+  ball.style.left = x + 'px';
+  ball.style.top = y + 'px';
+  
+
+  ball.style.transform = 'translate(-50%, -50%)'; 
+});
